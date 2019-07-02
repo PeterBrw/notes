faf81457d5c25ee8bad5f1914f3cd93d1a1bebd0
PeterBrw <peter.giurgiu@gmail.com>
2019-07-02 23:05:34 +0100

    adding-more-information-about-lecture-23

--- main.go
+++ main.go
@@ -190,7 +190,14 @@ func main() {
 	// fmt.Printf("%T\n", z)
 }
 
-
+// primitive data types
+// In computer science, primitive data type is either of the following: 
+// a basic type is a data type provided by a programming language as a basic building block. Most languages allow more complicated composite types to be constructed starting from basic types. 
+// a built-in type is a data type for which the programming language provides built-in support. 
+// In most programming languages, all basic data types are built-in. In addition, many languages also provide a set of composite data types. Opinions vary as to whether a built-in type that is not basic should be considered "primitive".
+// https://en.wikipedia.org/wiki/Primitive_data_type 
+// composite data types
+// In computer science, a composite data type or compound data type is any data type which can be constructed in a program using the programming language's primitive data types and other composite types. It is sometimes called a structure or aggregate data type, although the latter term may also refer to arrays, lists, etc. The act of constructing a composite type is known as composition
 
 
 // 24. Zero value
